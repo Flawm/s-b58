64e5940a0821f35b55166b516f96b0ad00114018
vlawmz <flawmz@gmail.com>
2022-08-07 12:06:49 -0600

    Get magic to work

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,40 @@
-#![feature(const_for)]
+use {clap::Parser, std::io, std::io::prelude::*};
 
-use {
-    clap::Parser,
-    std::io,
-    std::io::prelude::*,
-    std::collections::HashMap
-};
-
-macro_rules! str_map {
-    ($str:expr) => {
-        {
-            $str
-            .bytes()
-            .enumerate()
-            .map(|(i, j)| (j, i))
-            .collect::<HashMap<u8, usize>>()
+macro_rules! flip_alpha {
+    ( $lookup:expr ) => {{
+        let mut a: [u8; 128] = [255; 128];
+        let mut i = 0;
+        loop {
+            if i < 58 {
+                a[$lookup[i] as usize] = i as u8;
+            } else if i == 128 {
+                break a;
+            }
+            i += 1;
         }
-    };
+    }};
 }
 
-macro_rules! str_map2 {
-    ($bytes:expr) => {
-        {
-            let mut a: [u8; 255] = [0; 255];
-            for x in b"123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz" {
-                a[*x as usize] = 1;
-            };
-
-            a
-        }
-    };
-}
-
-static ALPHABET:     [u8; 58] = *b"123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
-const ALPHABET_MAP: [u8; 255] = str_map2!(ALPHABET);
-
-thread_local!(
-    static B58_MAP: HashMap<u8, usize> = str_map!("1234vi6789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz");
-);
+const ALPHABET: [u8; 58] = *b"123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+const ALPHABET_MAP: [u8; 128] = flip_alpha!(ALPHABET);
 
 fn main() {
     match Args::parse().d {
         true => decode(),
-        _    => encode()
+        _ => encode(),
     }
 }
 
 fn decode() {
-    let mut input = 
-        io::stdin().bytes()
-        .map(|c| {
-            let c = c.unwrap();
-            
-            B58_MAP.with(|test| test.get(&c).unwrap().clone())
+    let mut input = io::stdin()
+        .bytes()
+        .enumerate()
+        .map(|(i, byte)| {
+            let byte = byte.unwrap();
+            match ALPHABET_MAP[byte as usize] as usize {
+                255 => panic!("Unmapped incoming byte {:?} at index {:?}.", byte, i),
+                any => any,
+            }
         })
         .collect::<Vec<usize>>();
 
@@ -60,8 +42,8 @@ fn decode() {
 
     input.iter_mut().enumerate().for_each(|(index, b58_index)| {
         out[..index].iter_mut().for_each(|digit| {
-            *b58_index  += (*digit as usize) * 58; // move up one sig digit
-            *digit       = (*b58_index & 0xff) as u8;
+            *b58_index += (*digit as usize) * 58; // move up one sig digit
+            *digit = (*b58_index & 0xff) as u8;
             *b58_index >>= 8;
         });
 
@@ -72,8 +54,7 @@ fn decode() {
     let out = &mut out[..parsed];
     out.reverse();
 
-//    io::stdout()
-//        .write(&out).unwrap();
+    io::stdout().write(&out).unwrap();
 }
 
 fn encode() {
@@ -85,7 +66,7 @@ fn encode() {
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-   /// To decode std::in
-   #[clap(short, long, action)]
-   d: bool,
+    /// To decode std::in
+    #[clap(short, long, action)]
+    d: bool,
 }
